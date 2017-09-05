a8138aa0ea3d042bc0a094a29376d3f4f042fb45
WeiWeiJump <zw9095@gmail.com>
2017-09-05 19:03:26 -0400

    String

---
+++ String/383_Ransom_Note.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Ransom Note
+ * Number: 383
+ * Tag: String
+ * Time Comlexity: O(magazine.lenth + ransomNote.len)
+ * Space Complexity: O(1)
+**/
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (count[ransomNote.charAt(i) - 'a']-- < 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
