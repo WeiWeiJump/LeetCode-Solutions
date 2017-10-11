ac9e7b16aeb2212e1f435fab71877e345df93bb4
WeiWeiJump <zw9095@gmail.com>
2017-10-11 14:03:33 -0400

    dfs

--- DFS/Matrix/529_Minesweeper.java
+++ DFS/Matrix/529_Minesweeper.java
@@ -1,48 +1,73 @@
-/**Name: Minesweeper
+/**
+ * Name: Minesweeper
  * Number: 529
- * Tag: DFS/BFS
- * Time Complexty: O(n)
+ * Tag: DFS
+ * Main Points:
+   first, we only need to do with the case when clicking on 'M' or 'E'
+   if user click on anything revealed ('B' or digit or 'X'), give no response
+   1. if click on 'M', change 'M' to 'X'
+   2. if click on 'E', count adjacent mines
+      1. if there is no adj mines, DFS all its adj mines
+      2. if there is adj mines, expose it
+ * Time Complexity: O(n)
+ * Space Complexity: worst O(n)
 **/
-public class Solution {
+class Solution {
     public char[][] updateBoard(char[][] board, int[] click) {
-        int m = board.length, n = board[0].length;
-        int count = 0;
-        int row = click[0], column = click[1];
+        //throw exception here is better
+        if (board == null || board.length == 0 || board[0].length == 0 || click == null || click.length == 0) {
+            return new char[][]{{}};
+        }
         
-        //1.If click on a mine ('M'), mark it as 'X', stop further search.
-        if (board[row][column] == 'M') {
-            board[row][column] = 'X';
-            //2.If click on an empty cell ('E'), depends on how many surrounding mine:
-        } else if (board[row][column] == 'E') {
-        	//count the number of surrounding mines;
-            for (int i = -1; i < 2; i++) {
-                for (int j = -1; j < 2; j++) {
-                    if (j == 0 && i == 0) continue;
-                    int r = row + i, c = column + j;
-                    if (r < 0 || r >= m || c < 0 || c >= n) continue;
-                    if(board[r][c] == 'M') {
-                        count++;
-                    }
-                }
-            }
-            //2.1 Has surrounding mine(s), mark it with number of surrounding mine(s), stop further search.
-            if (count != 0) {
-                board[row][column] = (char)(count + '0');
-            //2.2 No surrounding mine, mark it as 'B', continue search its 8 neighbors.
+        char clicked = board[click[0]][click[1]];
+        if (clicked == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        if (clicked == 'E') {
+            //count adjacent mines
+            int count = adjMines(board, click[0], click[1]);
+            if (count > 0) {
+                board[click[0]][click[1]] = (char)(count + '0');
+                return board;
             } else {
-                board[row][column] = 'B';
-                for (int i = -1; i < 2; i++) {
-                    for (int j = -1; j < 2; j++) {
-                        if (j == 0 && i == 0) continue;
-                        int r = click[0] + i, c = click[1] + j;
-                        if (r < 0 || r >= m || c < 0 || c >= n) continue;
-                        if (board[r][c] == 'E') {
-                            updateBoard(board, new int[]{r, c});
-                        }
-                    }
-                }
+                dfs(board, click[0], click[1], count);
+                return board;
             }
         }
         return board;
+    }
+    private int adjMines(char[][] board, int x, int y) {
+        int xLimit = board.length, yLimit = board[0].length;
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int curX = x + i, curY = y + j;
+                if (curX < 0 || curX >= xLimit || curY < 0 || curY >= yLimit || (i == 0 && j == 0)) {
+                    continue;
+                }
+                if (board[curX][curY] == 'M') {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    private void dfs(char[][] board, int x, int y, int count) {
+        int xLimit = board.length, yLimit = board[0].length;
+        if (x < 0 || x >= xLimit || y < 0 || y >= yLimit || board[x][y] != 'E') {
+            return;
+        }
+        if (count > 0) {
+            board[x][y] = (char)(count + '0');
+            return;
+        } 
+        
+        board[x][y] = 'B';
+        for (int[] dir : dirs) {
+            int nextX = x + dir[0], nextY = y + dir[1];
+            dfs(board, nextX, nextY, adjMines(board, nextX, nextY));
+        }     
     }
 }
