e83c43db0bdba8433172729bde09a3291b95cb70
WeiWeiJump <zw9095@gmail.com>
2017-11-02 15:57:53 -0400

    DFS

--- FaceBook/020_Valid_Parentheses.java
+++ FaceBook/020_Valid_Parentheses.java
@@ -16,11 +16,11 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 stack.push(s.charAt(i));
-            } else if (s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == ')') {
+            } else if (s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(') {
                 stack.pop();
-            } else if (s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '}') {
+            } else if (s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{') {
                 stack.pop();
-            } else if (s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == ']') {
+            } else if (s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[') {
                 stack.pop();
             } else {
                 return false;
