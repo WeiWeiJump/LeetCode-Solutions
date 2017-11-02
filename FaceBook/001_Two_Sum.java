4ab598c0a7a453019abab5aa5b4bad7343652b3b
WeiWeiJump <zw9095@gmail.com>
2017-11-02 10:36:16 -0400

    add Follow up

--- FaceBook/001_Two_Sum.java
+++ FaceBook/001_Two_Sum.java
@@ -3,10 +3,17 @@
  * Number: 1
  * Tag: Array
  * Main Points: use a hashmap to store
+   2 ways come into mind:
+   (1) brute force (TC:O(n^2), SC:O(1))
+   (3) HashMap (TC: O(n), SC:O(n))
  * Time Complexity: O(n)
  * Space Complexity: O(n)
+ * Follow up: 
+   if we need to output the numbers rather than indices
+   hashmap works, 2 pointers also works(TC: O(nlogn), SC:O(1))
 **/
 class Solution {
+    /******************** HashMap ********************/
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         if (nums == null || nums.length == 0) return ans;
@@ -21,4 +28,5 @@ class Solution {
         }
         return ans;    
     }
+    
 }
