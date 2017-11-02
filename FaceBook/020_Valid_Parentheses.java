bca8de6e2c3643c8d2768106172857ad061e5da5
WeiWeiJump <zw9095@gmail.com>
2017-11-02 15:56:26 -0400

    Stack

---
+++ FaceBook/020_Valid_Parentheses.java
@@ -0,0 +1,31 @@
+/**
+ * Name: Valid Parentheses
+ * Number: 20
+ * Tag: Stack
+ * Main Points: 
+   if current char is left half of parenthesis, we should push it into the stack
+   if current char is right half, we should check whether it's the same with stack.peek()
+   if they are not same like [}, then return false
+   if all right half finds it's left half, but final stack is not empty, like [[], then return false;
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+**/
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == ')') {
+                stack.pop();
+            } else if (s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '}') {
+                stack.pop();
+            } else if (s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == ']') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
