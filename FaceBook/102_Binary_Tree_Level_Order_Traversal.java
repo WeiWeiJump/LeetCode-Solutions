78f5e35eea78726afc9ea6e2a60bdf59ddfabe3c
WeiWeiJump <zw9095@gmail.com>
2017-11-04 19:16:03 -0400

    BFS

---
+++ FaceBook/102_Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,41 @@
+/**
+ * Name: Binary Tree Level Order Traversal
+ * Number: 102
+ * Tag: BFS
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+**/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                level.add(cur.val);
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
