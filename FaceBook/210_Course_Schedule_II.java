5ccf8475f331d8907e93e12208d47779542a6d6a
WeiWeiJump <zw9095@gmail.com>
2017-11-25 10:09:58 -0500

    BFS

--- FaceBook/210_Course_Schedule_II.java
+++ FaceBook/210_Course_Schedule_II.java
@@ -5,46 +5,46 @@
  * Main Points: 
    List[] to represent adj list
    indegree[] to represent each node's indegree
- * Time Complexity: O(n)
+ * Time Complexity: O(V+E+V+V*E+V) E = V - 1(no circle) E = V(circle) -> O(V*V)
  * Space Complexity: O(n)
 **/
+//0:{1,2} 0
+//1:{3}   1
+//2:{3}   1
+//3:{}    2
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adjList = new List[numCourses];
+        List<Integer>[] nextCourse = new List[numCourses];
         int[] indegree = new int[numCourses];
-        
         for (int i = 0; i < numCourses; i++) {
-            adjList[i] = new ArrayList<Integer>();
+            nextCourse[i] = new ArrayList<Integer>();
         }
-        
         for (int[] prerequisite : prerequisites) {
-            int toStudy = prerequisite[0], pre = prerequisite[1];   
-            adjList[pre].add(toStudy);
-            indegree[toStudy]++;
+            nextCourse[prerequisite[1]].add(prerequisite[0]);
+            indegree[prerequisite[0]]++;
         }
-        
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
                 queue.offer(i);
             }
-        }
-        List<Integer> res = new ArrayList<>();
+        } 
+        List<Integer> courseList = new ArrayList<>();
         while (!queue.isEmpty()) {
             int curCourse = queue.poll();
-            res.add(curCourse);
-            for (int toStudy : adjList[curCourse]) {
-                if (--indegree[toStudy] == 0) {
-                    queue.offer(toStudy);
+            courseList.add(curCourse);
+            for (int next : nextCourse[curCourse]) {
+                if(--indegree[next] == 0) {
+                    queue.offer(next);
                 }
             }
         }
-        if (res.size() != numCourses) return new int[0];
-        int[] ret = new int[numCourses];
-        int j = 0;
-        for (int i : res) {
-            ret[j++] = i;
+        if (courseList.size() != numCourses) return new int[0];
+        int[] res = new int[courseList.size()];
+        int i = 0;
+        for (int course : courseList) {
+            res[i++] = course;
         }
-        return ret;
+        return res;
     }
 }
